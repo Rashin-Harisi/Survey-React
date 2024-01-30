7158a748f516bbe998bd57d5f6a7b60e0c600900
Rashin Harisi <rashin.harisi@gmail.com>
2024-01-30 12:20:24 +0100

    creating the first page of the survey to get the information of users.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 
+import { useState } from 'react';
 import './App.css';
+import Questions from './components/questions/questions';
 
 function App() {
+  const [value, setValue] = useState('');
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  const handleChange = (event) => {
+    if (event) event.preventDefault();
+    setValue(event.target.value)
+  }
+  const handleClick = () => {
+    alert(`Dear ${value}, welcome to the survey. Please note that you have 2 minutes to answer each questions.`);
+    setButtonClicked(true);
+  }
+
+
   return (
     <div className="App">
-      Survey Website
+      {!buttonClicked ? (
+        <>
+          <div className='welcome'>The survey is about the Psychology topic. </div>
+          <div className='info'>
+            <form>
+              <label form='name'>Please Enter Your Full-Name: </label>
+              <input className='name' id='name' type='text' required placeholder='e.x Rashin Harisi' onChange={handleChange} />
+              <label form='age'>Please Enter Your Age: </label>
+              <input className='age' id='age' type='text' required placeholder='e.x 33' />
+              <input className='submit' type='submit' value='Start' onClick={handleClick} />
+            </form>
+          </div>
+        </>
+      ) : (<Questions />)
+      }
+
+
+
+
     </div>
   );
 }
---
+++ src/components/questions/questions.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Questions =() =>{
+    return(
+        <div>questions</div>
+    )
+} ;
+
+export default Questions;
