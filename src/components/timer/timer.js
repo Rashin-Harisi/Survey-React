4a84c1669fdfff56042c6e3ac40da9b102ac6180
Rashin Harisi <rashin.harisi@gmail.com>
2024-01-31 12:03:07 +0100

    creating timer component

--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './timer.css';
+//import alertSound from '../../../public/sound.mp3';
 
 
-const Timer= ()=>{
-    return(
-        <div>Timer part</div>
+const Timer = () => {
+    const [start, setStart] = useState(false);
+    const [timerFinished, setTimerFinished] = useState(false);
+    const [count, setCount] = useState(0);
+    const [audio]=useState(new Audio(`${process.env.PUBLIC_URL}/sound.mp3`));
+    const [soundPlayed, setSoundPlayed] = useState(false);
+
+
+    const [minutes1, setMinutes1] = useState(2);
+    const [minutes2, setMinutes2] = useState(0);
+
+    const [seconds1, setSeconds1] = useState(0);
+    const [seconds2, setSeconds2] = useState(0);
+
+    useEffect(() => {
+        setStart(true)
+    }, []);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCount(count + 1)
+            if (seconds1 > 0) {
+                setSeconds1(seconds1 - 1);
+                return
+            }
+            if (seconds2 > 0) {
+                setSeconds2(seconds2 - 1);
+                setSeconds1(9);
+                return
+            }
+
+            if (minutes1 > 0) {
+                setMinutes1(minutes1 - 1);
+                setSeconds1(9);
+                setSeconds2(5);
+                return
+            }
+
+            if (minutes2 > 0) {
+                setMinutes2(minutes2 - 1);
+                setMinutes1(9);
+                setSeconds1(9);
+                setSeconds2(5);
+                return
+            }
+            setTimerFinished(true);
+        }, 1000)
+    }, [start, count]);
+
+    useEffect(() => {
+        if (timerFinished && !soundPlayed) {
+            audio.play();
+            setSoundPlayed(true);
+        }
+    }, [timerFinished, soundPlayed, audio]);
+
+    useEffect(()=>{
+       if(timerFinished && soundPlayed){
+        alert('Your time is finished.')
+       } 
+    },[timerFinished, soundPlayed]);
+
+    return (
+
+        <div className='timer'>
+            <div className='minutes'>
+                <input type='button' value={minutes2} />
+                <input type='button' value={minutes1} />
+            </div>
+            <span className='space'>:</span> 
+            <div className='seconds'>
+                <input type='button' value={seconds2} />
+                <input type='button' value={seconds1} />
+            </div>
+        </div>
+
+
+
     )
 };
 
